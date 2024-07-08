863f4f9924f097cab5f8d483de4abbbd4a48a5d5
Helder Oliveira <heldrida@gmail.com>
2024-07-08 15:28:23 +0100

    refactor: 💡 directory exists

--- src/commands/sites/utils/directoryExists.ts
+++ src/commands/sites/utils/directoryExists.ts
@@ -1,13 +1,18 @@
 import { promises as fs } from "node:fs";
 
+interface FsError extends Error {
+	code?: string;
+}
+
 export const directoryExists = async (path: string) => {
 	try {
 		const stat = await fs.stat(path);
 
 		return stat.isDirectory();
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	} catch (e: any) {
-		if (e.code === "ENOENT") {
+	} catch (e) {
+		const err = e as FsError;
+		if (err.code === "ENOENT") {
 			return false;
 		}
 
