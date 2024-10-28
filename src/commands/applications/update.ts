5d4ce4fcf2c76257608ab138f5b22337df9ffeea
Helder Oliveira <heldrida@gmail.com>
2024-10-28 10:31:18 +0000

    refactor: 💡 use prompt until utility for get whitelist domains

--- src/commands/applications/update.ts
+++ src/commands/applications/update.ts
@@ -1,3 +1,4 @@
+import { isHostnameValid } from '@fleek-platform/utils-validation';
 import { output } from '../../cli';
 import type { SdkGuardedFunction } from '../../guards/types';
 import { withGuards } from '../../guards/withGuards';
@@ -5,6 +6,7 @@ import { t } from '../../utils/translation';
 import { enterApplicationNameOrPrompt } from './prompts/enterApplicationNameOrPrompt';
 import { getApplicationOrPrompt } from './prompts/getApplicationOrPrompt';
 import { getWhitelistDomainsOrPrompt } from './prompts/getWhitelistDomainsOrPrompt';
+import { promptUntil } from '../../utils/prompts/promptUntil';
 
 type UpdateApplicationArgs = {
   id?: string;
@@ -28,11 +30,29 @@ const updateApplicationAction: SdkGuardedFunction<
     application,
   });
 
-  const whitelistDomains = await getWhitelistDomainsOrPrompt({
+  const handler = async () => getWhitelistDomainsOrPrompt({
     whitelistDomains: args.whitelistDomains,
     whitelistDomainsToUpdate: application.whitelistDomains.map(
       (whitelistDomain) => whitelistDomain.hostname,
     ),
+  });
+
+  const validator = async (data: string[]) => {
+    let hasInvalidHostname = false;
+
+    for (const hostname of data) {
+      if (!isHostnameValid({ hostname })) {
+        hasInvalidHostname = true;
+        output.warn(t('invalidHostname', { hostname }));
+      }
+    }
+
+    return !hasInvalidHostname;
+  }
+
+  const whitelistDomains = await promptUntil({
+    handler,
+    validator,
   });
 
   // Warning: The WhiteLabelDomains has been deprecated
